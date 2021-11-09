3682b1ea096b2397eb1ee7f59040bef651561652
alk3pInjection <webmaster@raspii.tech>
2021-11-09 14:32:18 +0800

    drm: Handle dim for udfps
    * Apparently, los fod impl is better than udfps cuz it
      has onShow/HideFodView hook, which allows us to toggle
      dimlayer seamlessly.
    
      Since udfps only partially supports the former one,
      we'd better kill dim in kernel. This is kinda a hack
      but it works well, bringing perfect fod experience
      back to us.
    
    Co-authored-by: Art_Chen <Chenxy0201@qq.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: I80bfd508dacac5db89f4fff0283529c256fb30ce

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -4332,6 +4332,11 @@ int dsi_panel_post_switch(struct dsi_panel *panel)
 bool aod_fod_flag;
 bool aod_complete;
 bool real_aod_mode;
+
+extern bool oneplus_dimlayer_hbm_enable;
+bool backup_dimlayer_hbm = false;
+extern int oneplus_dim_status;
+int backup_dim_status = 0;
 int dsi_panel_enable(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -4364,6 +4369,10 @@ int dsi_panel_enable(struct dsi_panel *panel)
 
 	panel->panel_initialized = true;
 	pr_err("dsi_panel_enable aod_mode =%d\n", panel->aod_mode);
+
+	oneplus_dimlayer_hbm_enable = backup_dimlayer_hbm;
+	oneplus_dim_status = backup_dim_status;
+	pr_err("Restore dim when panel goes on");
 
 	if (panel->aod_mode == 0) {
 		pr_err("Send dsi_panel_set_aod_mode 0 cmds\n");
@@ -4474,6 +4483,9 @@ int dsi_panel_disable(struct dsi_panel *panel)
 
 	/* Avoid sending panel off commands when ESD recovery is underway */
 	if (!atomic_read(&panel->esd_recovery_pending)) {
+		oneplus_dimlayer_hbm_enable = false;
+		oneplus_dim_status = 0;
+		pr_err("Kill dim when panel goes off");
 		HBM_flag = false;
 
 		if (panel->aod_mode == 2)
