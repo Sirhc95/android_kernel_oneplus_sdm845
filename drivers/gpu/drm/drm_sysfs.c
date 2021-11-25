5d89a653eed3bdb645863ebcd2a70c57f02c0e12
alk3pInjection <webmaster@raspii.tech>
2021-11-25 12:36:33 +0800

    drm: Expose power status to userspace
    * supplementary to commit ("drm: Implement panel status check")
    
    Change-Id: Id011c584299219e5364deae62d6786de471fce01

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -885,6 +885,15 @@ static ssize_t op_display_get_power_status(struct device *dev,
 {
 	return sprintf(buf, "%d\n", oneplus_panel_status);
 }
+
+static ssize_t op_display_set_power_status(struct device *dev,
+				struct device_attribute *attr,
+				const char *buf, size_t count)
+{
+	sscanf(buf, "%d", &oneplus_panel_status);
+
+	return count;
+}
 }
 /******************************************************************/
 static DEVICE_ATTR_RW(status);
@@ -925,6 +934,7 @@ static DEVICE_ATTR(dimlayer_bl_en, S_IRUGO|S_IWUSR,
 
 static DEVICE_ATTR_RW(dsi_panel_command);
 static DEVICE_ATTR_RW(dsi_seed_command);
+static DEVICE_ATTR(power_status, S_IRUGO|S_IWUSR, op_display_get_power_status, op_display_set_power_status);
 
 static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_status.attr,
@@ -955,6 +965,7 @@ static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_dimlayer_bl_en.attr,
 	&dev_attr_dp_en.attr,
 	&dev_attr_dither_en.attr,
+	&dev_attr_power_status.attr,
 	NULL
 };
 
